cac1d2ed07329f40155a7a2d4f8b6135d522c0ae
Cole McKee <30710889+metalmulisha205@users.noreply.github.com>
2019-11-08 08:57:11 -0600

    fixed styling
    There is still some work to do to make it more scalable.

--- src/Components/About.js
+++ src/Components/About.js
@@ -16,9 +16,195 @@ export default function About() { // no state needed so functional component use
     return (
         <div className="About">
             <div className="header">
-                <p>Meet the Team</p>
+                <p 
+                    style={
+                        {
+                            marginTop:20,
+                        }
+                    }
+                >
+                    Meet the Team
+                </p>
             </div>
-            <Row>
+            <div className = "container"
+            style={
+                {
+                    display: "flex",
+                    flexDirection:"row",
+                    justifyContent: "center",
+                    width: "120%",
+                    //backgroundColor: "red",
+                }
+            }
+            >
+                <div className = "container" 
+                stle={
+                    {
+                        flex:1,
+                        display: "flex",
+                        top: 10,
+                        justifyContent: "center",
+                    }
+                }
+                >
+                    <Col span={12}
+                        style={
+                            {
+                                padding: 10,
+                                width: 700,
+                                height: 800,
+                                marginLeft: 440,
+                            }
+                        }
+                    >
+                        <Card
+                            hoverable={true}
+                            cover={
+                                <img
+                                    alt="example"
+                                    src={team_comp}
+                                />
+                            }
+                        >
+                            <Meta
+                                title="JCIB ForensX"
+                                description="The JCIB ForensX team was founded in 2017 as a school team housed at the Jefferson County International Baccalaureate (JCIB) school.
+                                After recognizing a lack of Computer Science-focused extracurricular activities at the JCIB program, the team formed with the directive of increasing Computer Science engagement within the student body.
+                                The ForensX team looks to leverage emerging technology for the positive development of our community."
+                            />
+                        </Card>
+                    </Col>
+                </div>
+                <div className = "container"
+                    style={
+                        {
+                            flex:1,
+                            display:"flex",
+                            top: 10,
+                            marginLeft: 20,
+                        }
+                    }
+                >
+                    <Col span={12}
+                    style={
+                        {
+                            padding: 10,
+                            width: 900,
+                            height: 800
+                        }
+                    }>
+                    <Row>
+                        <Col span={6}>
+                            <Card
+                                style={
+                                    {
+                                        padding: 10
+                                    }
+                                }
+                                hoverable={true}
+                                cover={
+                                    <img
+                                        alt="Aniket Pant"
+                                        src={aniket_solo}
+                                    />
+                                }
+                                actions={[
+                                    <Icon type="setting" key="setting" />,
+                                    <Icon type="edit" key="edit" />,
+                                ]}
+                            >
+                                <Meta
+                                    avatar={<Avatar src="https://zos.alipayobjects.com/rmsportal/ODTLcjxAfvqbxHnVXCYX.png" />}
+                                    title="Aniket Pant"
+                                    description="Data Analytics, Visualization"
+                                />
+                            </Card>
+                        </Col>
+                        <Col span={6}>
+                            <Card
+                                style={
+                                    {
+                                        padding: 10
+                                    }
+                                }
+                                hoverable={true}
+                                cover={
+                                    <img
+                                        alt="example"
+                                        src={cole_solo}
+                                    />
+                                }
+                                actions={[
+                                    <Icon type="setting" key="setting" />,
+                                    <Icon type="edit" key="edit" />,
+                                ]}
+                            >
+                                <Meta
+                                    avatar={<Avatar src="https://zos.alipayobjects.com/rmsportal/ODTLcjxAfvqbxHnVXCYX.png" />}
+                                    title="Cole McKee"
+                                    description="UI/UX, Web Development"
+                                />
+                            </Card>
+                        </Col>
+                    </Row>
+                    <Row>
+                        <Col span={6}>
+                            <Card
+                                style={
+                                    {
+                                        padding: 10
+                                    }
+                                }
+                                hoverable={true}
+                                cover={
+                                    <img
+                                        alt="example"
+                                        src={viraj_solo}
+                                    />
+                                }
+                                actions={[
+                                    <Icon type="setting" key="setting" />,
+                                    <Icon type="edit" key="edit" />,
+                                ]}
+                            >
+                                <Meta
+                                    avatar={<Avatar src="https://zos.alipayobjects.com/rmsportal/ODTLcjxAfvqbxHnVXCYX.png" />}
+                                    title="Viraj Kacker"
+                                    description="Arizona Plane Ticket Owner"
+                                />
+                            </Card>
+                        </Col>
+                        <Col span={6}>
+                            <Card
+                                style={
+                                    {
+                                        padding: 10
+                                    }
+                                }
+                                hoverable={true}
+                                cover={
+                                    <img
+                                        alt="example"
+                                        src={lonnie_solo}
+                                    />
+                                }
+                                actions={[
+                                    <Icon type="setting" key="setting" />,
+                                    <Icon type="edit" key="edit" />,
+                                ]}
+                            >
+                                <Meta
+                                    avatar={<Avatar src="https://zos.alipayobjects.com/rmsportal/ODTLcjxAfvqbxHnVXCYX.png" />}
+                                    title="Lonnie Webb"
+                                    description="Predictive Modeling"
+                                />
+                            </Card>
+                        </Col>
+                    </Row>
+                </Col>
+                </div>
+            </div>
+            {/* <Row>
                 <Col span={12}
                     style={
                         {
@@ -42,7 +228,7 @@ export default function About() { // no state needed so functional component use
                             description="The JCIB ForensX team was founded in 2017 as a school team housed at the Jefferson County International Baccalaureate (JCIB) school.
                             After recognizing a lack of Computer Science-focused extracurricular activities at the JCIB program, the team formed with the directive of increasing Computer Science engagement within the student body.
                             The ForensX team looks to leverage emerging technology for the positive development of our community."
-  />
+                        />
                     </Card>
                 </Col>
                 <Col span={12}
@@ -162,7 +348,7 @@ export default function About() { // no state needed so functional component use
                         </Col>
                     </Row>
                 </Col>
-            </Row>
+            </Row> */}
             <Link to={process.env.PUBLIC_URL + '/'}>
                 <button className="homeButton">Back to home page</button>
             </Link>
